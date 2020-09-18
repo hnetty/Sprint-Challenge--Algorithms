4ee9da1c6bdec9955005b326861a8abae23cc5b9
Harper Nettesheim <harpernettesheim@gmail.com>
2020-09-18 11:06:31 -0500

    count problem solved

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,17 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    # Automatically checks to see if 'th' can even be in the parameter given to us
+    if len(word) < 2:
+        return 0
+
+
+    # checks the last two portions of the string, and adds 1 to the counter and recurses & slices the last two positions
+    if word[-2] == "t" and word[-1] == "h":
+        return 1 + count_th(word[:-2])
+
+    # calls the function again if the last two letters aren't 'th', and takes out the last positon for the next round through
+    else:
+        return count_th(word[:-1])
     
     pass
